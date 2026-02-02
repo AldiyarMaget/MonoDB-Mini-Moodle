b7f7f41c16d4b14ed555eba038df9e1a15e5131e
AldiyarMaget <241527@astanait.edu.kz>
2026-02-02 12:27:08 +0500

    Fix

--- routes/Route.go
+++ routes/Route.go
@@ -9,4 +9,9 @@ import (
 func RegisterRoutes() {
 	http.HandleFunc("/register", handlers.Register)
 	http.HandleFunc("/login", handlers.Login)
+	http.HandleFunc("GET /course/{id}", handlers.GetCourse)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("GET /home", handlers.Home)
 }
